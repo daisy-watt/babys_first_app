b0e458acf8057655dd3b83e34f56e47ed0cb087a
cloth-code <CAM022117@coderacademy.edu.au>
2021-12-15 23:26:12 +1100

    adding choice for a randomize colour feature

--- main.rb
+++ main.rb
@@ -1,6 +1,9 @@
 require "json"
 require "paint"
 
+#MENU - has option to select things
+
+
 # Read the file from disk into a string.
 structure_file_string = File.read('./checker_structure.json')
 # Convert the JSON string into a ruby hash.
@@ -9,21 +12,32 @@ structure_hash = JSON.parse(structure_file_string)
 puts "Please tell me your name"
 name = gets.chomp.downcase
 
-puts "Compose your weave by assigning the RGB values of the Warp and Weft"
+# -- could ask if the user wants random colorisation or specific colours
+puts "Would you like choice or no choice"
+choice = gets.chomp
 
-puts "Assign your Warp's Red color by choosing a value between 0-255"
-warp_color_R = gets.chomp.to_i
-puts "Assign your Warp's Green color by choosing a value between 0-255"
-warp_color_G = gets.chomp.to_i
-puts "Assign your Warp's Blue color by choosing a value between 0-255"
-warp_color_B = gets.chomp.to_i
-
-puts "Assign your Weft's Red color by choosing a value between 0-255"
-weft_color_R = gets.chomp.to_i
-puts "Assign your Weft's Green color by choosing a value between 0-255"
-weft_color_G = gets.chomp.to_i
-puts "Assign your Weft's Blue color by choosing a value between 0-255"
-weft_color_B = gets.chomp.to_i
+if choice == "no"
+    warp_color_R = rand(0..255)
+    warp_color_G = rand(0..255)
+    warp_color_B = rand(0..255)
+    weft_color_R = rand(0..255)
+    weft_color_G = rand(0..255)
+    weft_color_B = rand(0..255)
+else 
+    puts "Compose your weave by assigning the RGB values of the Warp and Weft"
+    puts "Assign your Warp's Red color by choosing a value between 0-255"
+    warp_color_R = gets.chomp.to_i
+    puts "Assign your Warp's Green color by choosing a value between 0-255"
+    warp_color_G = gets.chomp.to_i
+    puts "Assign your Warp's Blue color by choosing a value between 0-255"
+    warp_color_B = gets.chomp.to_i
+    puts "Assign your Weft's Red color by choosing a value between 0-255"
+    weft_color_R = gets.chomp.to_i
+    puts "Assign your Weft's Green color by choosing a value between 0-255"
+    weft_color_G = gets.chomp.to_i
+    puts "Assign your Weft's Blue color by choosing a value between 0-255"
+    weft_color_B = gets.chomp.to_i
+end
 
 # For each letter in the message string
 index = 0
@@ -32,9 +46,12 @@ while index < name.length
     character_in_name = name[index]  
     # Convert character to weave structure
     converted_character = structure_hash[character_in_name]
-    # Print out weave structure for chatacter
+    # Print out weave structure for chatacter ----- OPTIONS FOR "EXPORTING" (ie jpg, tff, pdf)
     puts Paint[converted_character, [warp_color_R, warp_color_G, warp_color_B], [weft_color_R, weft_color_G, weft_color_B]] 
     # puts converted_character.colorize(:background => warp_colour, :color => weft_colour)
     # Move to next character
     index = index + 1
 end
+
+# --- rand(0..255) for random colour generating 
+
