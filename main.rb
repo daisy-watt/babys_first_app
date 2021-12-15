e180d5d6a8b91520582b021057ae55c4a38b7892
cloth-code <CAM022117@coderacademy.edu.au>
2021-12-15 23:40:25 +1100

    crappy menu

--- main.rb
+++ main.rb
@@ -1,57 +1,15 @@
-require "json"
-require "paint"
+require_relative "make_weave"
 
-#MENU - has option to select things
+puts "hello this is a crappy menu, which way are you going to go?"
+puts "choose 0 to encrypt your name in a weave"
+puts "choose 1 to literally do anything else lol"
 
+choice = gets.chomp.to_i
 
-# Read the file from disk into a string.
-structure_file_string = File.read('./checker_structure.json')
-# Convert the JSON string into a ruby hash.
-structure_hash = JSON.parse(structure_file_string)
-# Get user input.
-puts "Please tell me your name"
-name = gets.chomp.downcase
-
-# -- could ask if the user wants random colorisation or specific colours
-puts "Would you like choice or no choice"
-choice = gets.chomp
-
-if choice == "no"
-    warp_color_R = rand(0..255)
-    warp_color_G = rand(0..255)
-    warp_color_B = rand(0..255)
-    weft_color_R = rand(0..255)
-    weft_color_G = rand(0..255)
-    weft_color_B = rand(0..255)
+if choice == 0
+    make_weave
+elsif choice == 1
+    puts "you are lost, go back"
 else 
-    puts "Compose your weave by assigning the RGB values of the Warp and Weft"
-    puts "Assign your Warp's Red color by choosing a value between 0-255"
-    warp_color_R = gets.chomp.to_i
-    puts "Assign your Warp's Green color by choosing a value between 0-255"
-    warp_color_G = gets.chomp.to_i
-    puts "Assign your Warp's Blue color by choosing a value between 0-255"
-    warp_color_B = gets.chomp.to_i
-    puts "Assign your Weft's Red color by choosing a value between 0-255"
-    weft_color_R = gets.chomp.to_i
-    puts "Assign your Weft's Green color by choosing a value between 0-255"
-    weft_color_G = gets.chomp.to_i
-    puts "Assign your Weft's Blue color by choosing a value between 0-255"
-    weft_color_B = gets.chomp.to_i
+    puts "how on earth did you get here"
 end
-
-# For each letter in the message string
-index = 0
-while index < name.length
-    # Get a single character from the message string
-    character_in_name = name[index]  
-    # Convert character to weave structure
-    converted_character = structure_hash[character_in_name]
-    # Print out weave structure for chatacter ----- OPTIONS FOR "EXPORTING" (ie jpg, tff, pdf)
-    puts Paint[converted_character, [warp_color_R, warp_color_G, warp_color_B], [weft_color_R, weft_color_G, weft_color_B]] 
-    # puts converted_character.colorize(:background => warp_colour, :color => weft_colour)
-    # Move to next character
-    index = index + 1
-end
-
-# --- rand(0..255) for random colour generating 
-
